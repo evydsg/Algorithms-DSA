2ec9c49be92589355bc46f7be9f776d54ab3f2f2
Evelise Karen Guenda <evelisekguenda@gmail.com>
2025-04-29 05:28:27 -0700

    removeDuplicates

---
+++ Arrays/Static Arrays/removeDuplicates/removeDuplicates.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0
+
+        for index in range(len(nums) - 1):
+            if nums[index] != nums[index + 1]:
+                nums[k] = nums[index]
+                k += 1
+            
+        nums[k] = nums[len(nums)- 1]
+        k += 1
+        return k
