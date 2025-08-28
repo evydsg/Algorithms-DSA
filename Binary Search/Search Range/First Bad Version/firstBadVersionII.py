124d010c3a983a977b4a4d9106b5d75bb0ddaa8b
Evelise Karen Guenda <evelisekguenda@gmail.com>
2025-08-28 05:16:12 -0700

    firstBadVersion

---
+++ Binary Search/Search Range/First Bad Version/firstBadVersionII.py
@@ -0,0 +1,16 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low, high = 1, n
+
+        while low <= high:
+            middle = (low+high)//2
+
+            if isBadVersion(middle) == False:
+                low = middle + 1
+            else:
+                high = middle - 1
+        
+        return low
