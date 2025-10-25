8be28e14743b362ac80d555928a415aee94fc95b
Evelise Karen Guenda <evelisekguenda@gmail.com>
2025-10-25 12:13:05 -0700

    Design Linked List

--- Linked Lists/Doubly Linked Lists/Design Linked List/linkedList.py
+++ Linked Lists/Doubly Linked Lists/Design Linked List/linkedList.py
@@ -2,82 +2,65 @@ class ListNode:
     def __init__(self, val, next = None) -> None:
        self.val = val
        self.next = None
+       self.prev = None
     
 class MyLinkedList:
-    def __init__(self, head):
-        self.head = None
+    def __init__(self):
+        #Dummy Nodes
+        self.left = ListNode(0)
+        self.right = ListNode(0)
+        self.left.next = self.right
+        self.right.prev = self.left
     
     def get(self, index):
-        if self.head is None or index == -1:
-            return -1
+        current = self.left.next
+
+        while current and index > 0:
+            currrent = current.next
+            index -= 1
         
-        count = 0
-        current = self.head
-
-        while current:
-            if count == index:
-                return current.val
-
-            count += 1
-            current = current.next
+        if current is not None and current != self.right and index == 0:
+            return current.val
         
         return -1
-    
     def addAtHead(self, val):
-        new_node = ListNode(val)
-        new_node.next = self.head
-        self.head = new_node
+        node, next, prev = ListNode(val), self.left.next, self.left
+
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
     
     def addAtTail(self, val):
-        new_node = ListNode(val)
+        node, next, prev = ListNode(val), self.right, self.right.prev
 
-        if self.head is None:
-            self.head = new_node
-            return
-        
-        current = self.head
-        
-        while current.next:
-            current = current.next
-        
-        current.next = new_node
-        current = current.next
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
     
     def addAtIndex(self, index, val):
-        if index == 0:
-            self.addAtHead(val)
-            return
+        current = self.left.next
         
-        new_node = ListNode(val)
-        count = 0
-        current = self.head
-
-        while current:
-            if count == index - 1:
-                new_node.next = current.next
-                current.next = new_node
-                return
-            
-            count += 1
+        while current and index > 0:
             current = current.next
+            index -= 1
         
-        if index == count:
-            self.addAtTail(val)
-            return
-        
+        if current and index == 0:
+            node, next, prev = ListNode(val), current, current.prev
+            prev.next = node
+            next.prev = node
+            node.next = next
+            node.prev = prev
 
     def deleAtIndex(self, index):
-        if index == 0 and self.head:
-            self.head = self.head.next
-            return 
-        
-        count = 0
-        current = self.head
+        current = self.left.next
 
-        while current and current.next:
-            if count == index - 1:
-                current.next = current.next.next()
-                return 
-            
+        while current and index > 0:
             current = current.next
-            count += 1
+            index -= 1
+
+        if current and current != self.right and index == 0:
+            next, prev = current.next, current.prev
+            next.prev = prev
+            prev.next = next
