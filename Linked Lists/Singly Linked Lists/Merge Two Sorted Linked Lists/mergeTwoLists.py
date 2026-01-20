1c81d5ccac2699f6a95272b749df7431260ebf47
Evelise Karen Guenda <evelisekguenda@gmail.com>
2026-01-20 15:42:46 +0200

    organized file

--- Linked Lists/Singly Linked Lists/Merge Two Sorted Linked Lists/mergeTwoLists.py
+++ Linked Lists/Singly Linked Lists/Merge Two Sorted Linked Lists/mergeTwoLists.py
@@ -3,7 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if list1 == None and list2 == None:
