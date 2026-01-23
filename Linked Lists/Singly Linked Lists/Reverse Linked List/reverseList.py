505e7704fcf144bb7a32933d0ef84ec54aaf0fa5
Evelise Karen Guenda <evelisekguenda@gmail.com>
2026-01-23 06:33:04 +0200

    Organized file

--- Linked Lists/Singly Linked Lists/Reverse Linked List/reverseList.py
+++ Linked Lists/Singly Linked Lists/Reverse Linked List/reverseList.py
@@ -3,7 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         pointer = head
